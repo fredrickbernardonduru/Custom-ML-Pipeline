0a428c539b3d933729c12cde5a0e4c4c69a2c224
Fredrick Bernard Onduru <fredrickbernardonduru@gmail.com>
2025-07-23 16:50:21 +0300

    Update

--- simple_pipeline.py
+++ simple_pipeline.py
@@ -7,3 +7,18 @@ class SimpleMLPipeline:
         #STEP 1: Initialize the components of the pipeline
         self.scaler = StandardScaler()
         self.model = LinearRegression()
+
+    def train(self, X_train, y_train):
+        #STEP 2: Fit the scaler and transform the training features
+        X_train = self.scaler.fit_transform(X_train)
+
+        #STEP 3: Train the regression model on th scaled data
+        self.model.fit(X_scaled, y_train)
+    
+    def predict(self, X):
+        # STEP 6: Transform input and return prediction
+        X_scaled = self.scaler.transform(X)
+        return self.model.predict(X_scaled)
+
+    def model_params(self):
+        return self.model.intercept_, self.model.coef_
