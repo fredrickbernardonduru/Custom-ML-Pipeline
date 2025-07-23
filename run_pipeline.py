306ab9d74cf88bbac40c4bd8aa4c63378aeb46ee
Fredrick Bernard Onduru <fredrickbernardonduru@gmail.com>
2025-07-23 16:55:49 +0300

    Update

--- run_pipeline.py
+++ run_pipeline.py
@@ -1,3 +1,8 @@
 form simple_pipeline import simple_pipeline
 X = [[1],[2],[3],[4],[5],[6]]
 y = [[10,20,30,40,50,60]]
+
+
+# Split the dataset
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
