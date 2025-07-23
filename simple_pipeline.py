79400b4cd00b486797c53ddf7d429fc8bcd238cb
Fredrick Bernard Onduru <fredrickbernardonduru@gmail.com>
2025-07-23 16:41:35 +0300

    Update

---
+++ simple_pipeline.py
@@ -0,0 +1,6 @@
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+
+class SimpleMLPipeline:
+    def __init__(self):
